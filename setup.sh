b3c43f989c3fa37acc7a1d80aff597d59fc63466
Wendy252004 <romeromich35@gmail.com>
2024-11-13 11:31:43 -0600

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,14 @@
+mkdir -p~/streamlit/
+echo "\
+[general]\n\
+
+email = \"romeromich35@gmail.com\"\n\
+"> ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+""> ~/.streamlit/credentials.toml
